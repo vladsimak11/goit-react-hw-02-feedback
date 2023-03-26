61b53cf556bb553237a3e90d303bd9bb57345453
Vladyslav Simak <simakv21@gmail.com>
2023-03-26 17:10:17 +0300

    Task done in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Hello, HW 2
-    </div>
-  );
-};
+import React, { Component } from "react";
+import css from './App.module.css';
+
+let total = 0;
+let positivePercentage = 0;
+
+const options = ['Good', 'Neutral', 'Bad'];
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleClick = (value) => {
+    value = value.toLowerCase();
+    console.log(value);
+    this.setState((prevState)=> {
+      return {
+        [value]: prevState[value] + 1,
+      }
+    })
+  }
+
+  countTotalFeedback = () => {
+    total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    positivePercentage = total && Math.round(this.state.good * 100 / total);
+    return positivePercentage;
+  }
+
+  render() {
+    const {good, neutral, bad} = this.state;
+    return (
+      <div className={css.block}>
+        <h1>Please leave feedback</h1>
+        <div className={css.listBtn}>
+          {options.map((option) => {
+
+            return (
+              <button onClick={() => this.handleClick(option)} key={option} className={`${css.itemBtn} ${css.itemBtn__green}`}>{option}</button>
+            )
+          })}
+        </div>
+        <h2>Statistics</h2>
+        {good || neutral || bad ? <ul className={css.list}>
+          <li className={css.item__green}>Good: {good}</li>
+          <li className={css.item__blue}>Neutral: {neutral}</li>
+          <li className={css.item__red}>Bad: {bad}</li>
+          <li className={css.item}>Total: {this.countTotalFeedback()}</li>
+          <li className={css.item}>Positive Feedback: {this.countPositiveFeedbackPercentage()}%</li>
+        </ul> : <span className={css.blockFeedBack}>There is no feedback</span>}
+      </div>
+    )
+  }
+}
+
+
+
+
+
